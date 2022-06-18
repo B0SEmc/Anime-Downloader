8f7eef938015f428c2c5b8b8967bab0cb14154bf
Thibaut <thibaut.musslin@gmail.com>
2022-06-18 11:36:15 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::process::Command;
+
+// get the link
+fn main() {
+    eprint!("Enter the m3u8 playlist link: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read link");
+    let link = input.trim();
+    downloader(link);
+}
+
+// download the playlist to mp4
+fn downloader(link: &str) {
+    eprint!("Please enter the name of the anime: ");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Failed to read name");
+    let name: &str = input2.trim();
+    let name_cmd: String = "-o ".to_owned() + name + ".mp4";
+    let mut cmd = Command::new("yt-dlp");
+    cmd.arg("--all-subs");
+    cmd.arg("-f mp4");
+    cmd.arg(name_cmd);
+    cmd.arg(link);
+    println!("Downloading... This should take 5-10 minutes");
+    match cmd.output() {
+        Ok(o) => unsafe {
+            println!("{}" ,String::from_utf8_unchecked(o.stdout));
+        },
+        Err(_e) => {
+            println!("An error occurred !");
+        }
+    }
+
+}
