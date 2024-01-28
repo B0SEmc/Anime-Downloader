c068540048ad218a4886147052e7bd0b4ddad621
Thibaut <thibaut.musslin@gmail.com>
2024-01-28 20:48:41 +0100

    first egui implementation

--- src/download.rs
+++ src/download.rs
@@ -1,9 +1,7 @@
-use indicatif::{ProgressBar, ProgressState, ProgressStyle};
-use std::fs;
+use std::path::Path;
 use std::path::PathBuf;
 use std::thread;
 use std::time::Duration;
-use std::{fmt::Write, path::Path};
 use ytd_rs::{Arg, YoutubeDL};
 
 use crate::config::Config;
@@ -36,29 +34,7 @@ pub fn download(url: &str, config: Config) -> Result<Config, &str> {
     let filepath = finalfile.clone() + ".part";
 
     if !check_file_exists(&filepath) {
-        return Err("Failed to start download");
+        return Err("Failed to start download, make sure you have yt-dlp installed and that the URL is correct.");
     }
-
-    println!("Starting download...");
-    let pb = ProgressBar::new(450000000); // average file size for 1080p anime episode since we can't get the file size from yt-dl
-    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-        .unwrap()
-        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
-        .progress_chars("#>-"));
-    while !pb.is_finished() {
-        if Path::new(&filepath).exists() {
-            pb.set_position(fs::metadata(&filepath).unwrap().len());
-        } else {
-            break;
-        }
-        thread::sleep(Duration::from_millis(20))
-    }
-
-    if fs::metadata(&finalfile).unwrap().len() < 1000000 {
-        return Err("File size too small, download most likely failed");
-    }
-
-    pb.set_length(fs::metadata(&finalfile).unwrap().len());
-    pb.finish_with_message("Done");
     Ok(config)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,112 @@
-use config::{get_config, Config};
+use config::{check_config_exists, get_config, no_config_found, Config};
+
+#[cfg(windows)]
+use winapi::um::wincon::GetConsoleWindow;
+use winapi::um::winuser::{ShowWindow, SW_HIDE};
+
 use download::download;
-use std::io::stdin;
+use eframe::egui;
+use std::{thread, time::Duration};
 
 mod config;
 mod download;
 
-fn main() {
-    let mut config: Config = get_config();
+#[derive(Default)]
+struct MyApp {
+    anime_link: String,
+    anime_name: String,
+}
 
-    eprint!("Enter the m3u8 playlist link: ");
-    let mut input = String::default();
-    stdin().read_line(&mut input).expect("Failed to read link");
-    let link = input.trim();
+fn do_config_stuff() -> Config {
+    let mut config: Config = get_config();
     config.episode_count += 1;
     config.save();
-    match download(link, config) {
-        Ok(config) => {
-            println!("Done! Press enter to exit");
-            if !config.auto_close {
-                stdin().read_line(&mut String::default()).unwrap();
+    config
+}
+
+fn main() {
+    if !check_config_exists() {
+        no_config_found()
+    }
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([480.0, 300.0]),
+        follow_system_theme: false,
+        default_theme: eframe::Theme::Light,
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Anime Downloader",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
+    .unwrap();
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        static mut STARTUP: bool = true;
+        if unsafe { STARTUP } {
+            unsafe {
+                STARTUP = false;
             }
-            std::process::exit(0);
+            ctx.set_pixels_per_point(1.2);
+            let mut style = egui::Style::default();
+            style.spacing.item_spacing = egui::vec2(6.0, 10.0);
+
+            ctx.set_style(style);
+            thread::spawn(move || {
+                thread::sleep(Duration::from_secs(1));
+                let window = unsafe { GetConsoleWindow() };
+                unsafe {
+                    ShowWindow(window, SW_HIDE);
+                }
+            });
         }
-        Err(e) => {
-            println!("{}", e);
-            println!("Press enter to exit");
-            stdin().read_line(&mut String::default()).unwrap();
-            std::process::exit(69);
-        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Anime Downloader");
+            ui.add(
+                egui::TextEdit::multiline(&mut self.anime_link)
+                    .hint_text("Anime link")
+                    .desired_width(400.0),
+            );
+            ui.horizontal(|ui| {
+                if ui.button("Download").clicked() {
+                    let link = self.anime_link.clone();
+                    println!("Downloading {}", link);
+                    thread::spawn(move || {
+                        download(link.trim(), do_config_stuff()).unwrap();
+                    });
+                }
+                if ui.button("Open config").clicked() {
+                    config::open_config();
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.label(format!("Episode count: {}", get_config().episode_count + 1));
+                if ui.button("➖").clicked() {
+                    let mut config = get_config();
+                    config.episode_count -= 1;
+                    config.save();
+                }
+                if ui.button("➕").clicked() {
+                    let mut config = get_config();
+                    config.episode_count += 1;
+                    config.save();
+                }
+                if ui.button("Reset").clicked() {
+                    config::set_episode_count(0);
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    egui::TextEdit::singleline(&mut self.anime_name)
+                        .hint_text(format!("Name: {}", get_config().name)),
+                );
+                if ui.button("Edit").clicked() {
+                    config::set_anime_name(self.anime_name.clone());
+                    self.anime_name = String::default();
+                }
+            });
+        });
     }
 }
